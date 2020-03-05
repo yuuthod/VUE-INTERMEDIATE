9ea1512f6ebf6117a0effd2c3e93753b47375bf3
yuuthod <yuuthod@gmail.com>
2020-03-05 17:52:39 +0900

    모듈화 1 : getters, mutations 파일 분리

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,49 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 Vue.use(Vuex);
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if(localStorage.length > 0) {
-            for (let i=1; i<localStorage.length; i++) {
-                if(localStorage.key(i) !== 'loglevel:webpack-dev-server' && localStorage.key(i) !== 'csCursors') {
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-        return arr;
-    }
-};
+
 
 export const store = new Vuex.Store({
     state: {
         todoItems: storage.fetch()
     },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations: {
-        addOneItem(state, todoitem) {
-            let obj = {completed: false, item:todoitem}
-            localStorage.setItem(todoitem,JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload) {
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1)
-        },
-        toggleOneItem(state, payload) {
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state) {
-            localStorage.clear();
-            state.todoItems = [];
-        }
-    }
+    getters,
+    mutations
 })
