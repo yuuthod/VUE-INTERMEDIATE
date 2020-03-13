c1ddb75319bf61a7e622dbbf7b981a614f03127b
yuuthod <yuuthod@gmail.com>
2020-03-13 23:56:45 +0900

    Router

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    Hello
+    <tool-bar></tool-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar'
 export default {
-  
+  components: {
+    ToolBar,
+  }
 }
 </script>
 
 <style>
+  body {
+    padding: 0;
+    margin: 0;
+  }
 </style>
---
+++ vue-news/src/components/ToolBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header>
+      <router-link to="/news">News</router-link>
+      <router-link to="/ask">Ask</router-link>
+      <router-link to="/jobs">Jobs</router-link>
+  </Header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  Header {
+    color: white;
+    background-color: #42b883;
+    display: flex;
+    padding: 15px 8px;
+  }
+  Header > a {
+    margin-right: 20px;
+    color: white;
+  }
+  Header > a:-webkit-any-link {
+    text-decoration: none;
+  }
+  Header > .router-link-exact-active {
+    color: #35495e;
+    border-bottom: 3px solid #35495e;
+  }
+</style>
