d2d6b47e76a955bddfbed6a113c6eeca4ca35bd6
yuuthod <yuuthod@gmail.com>
2020-03-14 19:52:52 +0900

    Add API

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>Ask</div>
+  <div>
+    <div v-for="item in ask" v-bind:key="item.id">{{ item.title }}</div>
+  </div>
 </template>
 
 <script>
+import { fetchAskList } from '../api/index.js'
 export default {
-
+  data() {
+    return{
+      ask: []
+    }
+  },
+  created() {
+    fetchAskList()
+    .then(response => this.ask = response.data)
+    .catch(error => console.log(error))
+  }
 }
 </script>
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
+import { fetchNewsList } from '../api/index.js'
 export default {
-
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    fetchNewsList()
+      .then(response => this.users = response.data)
+      .catch(error => console.log(error))
+  }
 }
 </script>
 
